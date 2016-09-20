362ddf2652a2f76345c54be8089f8eafabc88bbd
Insane-96 <insane96mcp@gmail.com>
2016-09-20 11:51:39 +0200

    Added changelog

--- engine.js
+++ engine.js
@@ -1,13 +1,3 @@
-/*
-	Added Layer system, with Game.LayerToName(layer), Game.NameToLayer(name) and Game.GetLayer(layer)
-		You can set the layer of a gameObject with GameObject.layer = layerNumber;
-		In config.js you can config the layer names, up to 31 names, defaults to ["player", "enemy"]
-		Layer -1 for game objects, means no layer
-	Added centerPosition property for every game object
-		returns the center position of the game object (Vector2(x + width / 2, y + height / 2))
-	Added config.js for some configurations
-	Renamed scaledSize in size and size in unscaledSize
-*/
 var Game = {
 	deltaTime: 0,
 	oldTime: 0,
